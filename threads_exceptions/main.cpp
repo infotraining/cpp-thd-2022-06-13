0f0d21d7c12efad24db4618435671da1aa23fc5b
Krystian Piękoś <krystian.piekos@infotraining.pl>
2022-06-13 15:38:05 +0200

    threads + exceptions

--- threads_exceptions/main.cpp
+++ threads_exceptions/main.cpp
@@ -1,3 +1,5 @@
+#include "joining_thread.hpp"
+
 #include <cassert>
 #include <chrono>
 #include <functional>
@@ -8,7 +10,22 @@
 
 using namespace std::literals;
 
-void background_work(size_t id, const std::string& text, std::chrono::milliseconds delay)
+template <typename T>
+struct ThreadResult
+{
+    T value;
+    std::exception_ptr eptr;
+
+    T get()
+    {
+        if (eptr)
+            std::rethrow_exception(eptr);
+
+        return std::move(value);
+    }
+};
+
+void background_work(size_t id, const std::string& text, std::chrono::milliseconds delay, ThreadResult<char>& result)
 {
     std::cout << "bw#" << id << " has started..." << std::endl;
 
@@ -19,40 +36,37 @@ void background_work(size_t id, const std::string& text, std::chrono::millisecon
         std::this_thread::sleep_for(delay);
     }
 
+    try
+    {
+        result.value = text.at(6);
+    }
+    catch (...)
+    {
+        result.eptr = std::current_exception();
+        return;
+    }
+
     std::cout << "bw#" << id << " is finished..." << std::endl;
 }
-
-class BackgroundWork
-{
-    const int id_;
-    const std::string text_;
-
-public:
-    BackgroundWork(int id, std::string text)
-        : id_{id}
-        , text_{std::move(text)}
-    {
-    }
-
-    void operator()(std::chrono::milliseconds delay) const
-    {
-        std::cout << "BW#" << id_ << " has started..." << std::endl;
-
-        for (const auto& c : text_)
-        {
-            std::cout << "BW#" << id_ << ": " << c << std::endl;
-
-            std::this_thread::sleep_for(delay);
-        }
-
-        std::cout << "BW#" << id_ << " is finished..." << std::endl;
-    }
-};
 
 int main()
 {
     std::cout << "Main thread starts..." << std::endl;
     const std::string text = "Hello Threads";
+
+    ThreadResult<char> result;
+
+    ext::joining_thread thd_1 {background_work, 1, text, 100ms, std::ref(result)};
+    thd_1.join();
+
+    try
+    {
+        std::cout << result.get() << " returned from thread" << std::endl;
+    }
+    catch (const std::out_of_range& e)
+    {
+        std::cout << "Logging " << e.what() << std::endl;
+    }
 
     std::cout << "Main thread ends..." << std::endl;
 }
