af9b60769d52e48183e396f810a13fbe0a31eb9c
Krystian Piękoś <krystian.piekos@infotraining.pl>
2022-06-23 09:40:54 +0200

    idle wait

--- event-synchronization/main.cpp
+++ event-synchronization/main.cpp
@@ -43,7 +43,6 @@ namespace BusyWaitWithMutex
 
         void consume(int id)
         {
-
             while (true)
             {
                 std::lock_guard<std::mutex> lk {mtx_is_data_ready_};
@@ -63,10 +62,11 @@ namespace BusyWaitWithAtomic
     {
         std::vector<int> data_;
         std::atomic<bool> is_data_ready_ {false};
+        static_assert(std::atomic<bool>::is_always_lock_free);
 
     public:
         void produce()
-        {                        
+        {
             std::cout << "Preparing data..." << std::endl;
             data_.resize(1000);
             std::random_device rd;
@@ -83,9 +83,6 @@ namespace BusyWaitWithAtomic
 
         void consume(int id)
         {
-            assert(is_data_ready_.is_lock_free());
-            static_assert(is_data_ready_.is_always_lock_free);
-
             // ###########################################
             while (!is_data_ready_.load(std::memory_order_acquire))
             {
@@ -97,9 +94,56 @@ namespace BusyWaitWithAtomic
     };
 }
 
+namespace IdleWait
+{
+    class Data
+    {
+        std::vector<int> data_;
+        bool is_data_ready_ {false};
+        std::mutex mtx_is_data_ready_;
+        std::condition_variable cv_data_ready_;
+
+    public:
+        void produce()
+        {
+            std::cout << "Preparing data..." << std::endl;
+            data_.resize(1000);
+            std::random_device rd;
+            std::mt19937_64 rnd_engine {rd()};
+            std::uniform_int_distribution<int> rnd_distr(0, 100);
+            std::generate(data_.begin(), data_.end(), [&]
+                { return rnd_distr(rnd_engine); });
+            std::this_thread::sleep_for(2s);
+            std::cout << "Data ready..." << std::endl;
+
+            {
+                std::lock_guard<std::mutex> lk{mtx_is_data_ready_};
+                is_data_ready_ = true;
+            }
+            cv_data_ready_.notify_all();            
+        }
+
+        void consume(int id)
+        {
+            std::unique_lock<std::mutex> lk{mtx_is_data_ready_};
+            cv_data_ready_.wait(lk, [this] { return is_data_ready_; });
+
+            // while (!is_data_ready_)
+            // {
+            //     cv_data_ready_.wait(lk);
+            // }
+            
+            lk.unlock();
+
+            int sum = std::accumulate(data_.begin(), data_.end(), 0);
+            std::cout << "Consumer#" << id << " - sum: " << sum << std::endl;
+        }
+    };
+}
+
 int main()
 {
-    using namespace BusyWaitWithMutex;
+    using namespace IdleWait;
 
     std::cout << "Main thread starts..." << std::endl;
     const std::string text = "Hello Threads";
